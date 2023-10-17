1a1d56525b8b71c1f9118e94e3f0235f60e86ae8
paulo.lima <paulolima18@gmail.com>
2023-10-17 10:35:48 +0100

    add model item

---
+++ app/src/main/java/com/paulolima/namegenerator/model/BabyNameItem.kt
@@ -0,0 +1,10 @@
+package com.paulolima.namegenerator.model
+
+data class BabyNameItem(
+    val yearOfBirth: String,
+    val gender: String,
+    val ethnicity: String,
+    val name: String,
+    val amount: String,
+    val rank: String,
+)
